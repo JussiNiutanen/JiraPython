f7e22e8a414135e9310570f3bb9510a47833ca67
Jussi Niutanen <jussi.niutanen@gmail.com>
2022-01-05 17:37:05 +0200

    Pylint updates 2

--- auto_sprint/as_even_main.py
+++ auto_sprint/as_even_main.py
@@ -8,20 +8,18 @@ from datetime import date
 
 from auto_sprints import MyData
 
-weekNumber = date.today().isocalendar()[1];
+weekNumber = date.today().isocalendar()[1]
 if(0==weekNumber%2):
-    print("Week number parillinen:", weekNumber);
+    print("Week number parillinen:", weekNumber)
 else:
-    print("Week number pariton:", weekNumber);
-    quit();
+    print("Week number pariton:", weekNumber)
+    quit()
 
 try:
     sys.argv[1], sys.argv[2]
 except IndexError:
-    print("\n TOO FEW PARAMETERS \n");
-    quit();
+    print("\n TOO FEW PARAMETERS \n")
+    quit()
    
-d = MyData(sys.argv[1],sys.argv[2]);
-d.update_sprints();
-
-
+d = MyData(sys.argv[1],sys.argv[2])
+d.update_sprints()
--- auto_sprint/auto_sprints.py
+++ auto_sprint/auto_sprints.py
@@ -30,8 +30,8 @@ class MyData:
     def debug_log(self,log_text, log_id =""):
 #        logging.debug(log_id);
 #        logging.debug(log_text);
-        print(log_id);
-        print(log_text);
+        print(log_id)
+        print(log_text)
 #        logging.debug(json.dumps(json.loads(log_text), sort_keys=True, indent=4, separators=(",", ": ")))
 #        print(log_id);
 #        print(json.dumps(json.loads(response_new_sprint.text), sort_keys=True, indent=4, separators=(",", ": ")))
@@ -43,58 +43,58 @@ class MyData:
         # Get all sprints
         response_get = requests.get(url, headers=self.c_accept_header, auth=(self.c_user, self.c_apikey))
         # Save position of the active text to id parameter.
-        res = response_get.text;
-        self.debug_log(json.dumps(json.loads(res), sort_keys=True, indent=4, separators=(",", ": ")),response_get);
+        res = response_get.text
+        self.debug_log(json.dumps(json.loads(res), sort_keys=True, indent=4, separators=(",", ": ")),response_get)
         
         # For some reason sometimes the request returns active sprint from other board. 
         # The next lines will filter sprints related to other boards away.
-        input_dict = json.loads(res);
-        output_dict = [x for x in input_dict['values'] if x['originBoardId'] == int(self.c_boar_id)];
+        input_dict = json.loads(res)
+        output_dict = [x for x in input_dict['values'] if x['originBoardId'] == int(self.c_boar_id)]
 
-        res = ' '.join(map(str, output_dict));
-        
-        active_pos = res.find(sprint_state);
+        res = ' '.join(map(str, output_dict))
     
-        id_postion = res.rfind("\'id\'", 0,active_pos);
+        active_pos = res.find(sprint_state)
+    
+        id_postion = res.rfind("\'id\'", 0,active_pos)
         # Remove not needed marks from the begining of the string
         sprint_id = res[id_postion+5::]
         # Copy string for later to get the name
-        sprint_name = sprint_id;
+        sprint_name = sprint_id
         # Remove not needed marks from the end of the sring
-        sprint_id = sprint_id[0:sprint_id.find(",")];
+        sprint_id = sprint_id[0:sprint_id.find(",")]
     
         # Save position of the sprint name text to id parameter.
-        name_position = sprint_name.find("\'name\'");
+        name_position = sprint_name.find("\'name\'")
         # Remove not needed marks from the begining of the string
         sprint_name = sprint_name[name_position+9::]
         # Remove not needed marks from the end of the sring
-        sprint_name = sprint_name[0:sprint_name.find("\'")];
+        sprint_name = sprint_name[0:sprint_name.find("\'")]
        
-        return sprint_id.strip(), sprint_name.strip();
+        return sprint_id.strip(), sprint_name.strip()
 
     """ Close active sprint """
     def close_sprint(self, sprint_id):
-        current_time_and_day = datetime.now().astimezone().replace(microsecond=0).isoformat();
-        url_close = self.c_jira_url + "/rest/agile/1.0/sprint/" + sprint_id;
+        current_time_and_day = datetime.now().astimezone().replace(microsecond=0).isoformat()
+        url_close = self.c_jira_url + "/rest/agile/1.0/sprint/" + sprint_id
         payload = json.dumps({
             "id": sprint_id,
             "self": url_close,
             "state": "closed",
             "completeDate": current_time_and_day
         })
-        response_close = requests.post(url_close, data=payload, headers=self.c_common_headers, auth=(self.c_user, self.c_apikey));
-        self.debug_log(json.dumps(json.loads(response_close.text), sort_keys=True, indent=4, separators=(",", ": ")),response_close);
+        response_close = requests.post(url_close, data=payload, headers=self.c_common_headers, auth=(self.c_user, self.c_apikey))
+        self.debug_log(json.dumps(json.loads(response_close.text), sort_keys=True, indent=4, separators=(",", ": ")),response_close)
 
     """ Create new sprint """
     def create_sprint(self, old_sprint_name):
-        current_time_and_day = datetime.now().astimezone().replace(microsecond=0);
-        end_time = current_time_and_day+ timedelta(days=7);
-        url_new_sprint = self.c_jira_url + "/rest/agile/1.0/sprint/";
-        name_len = len(old_sprint_name);
-        last_space = old_sprint_name.rfind(" ",0,name_len);
-        new_sprint_number = int(old_sprint_name[last_space:name_len].strip()) + 1;
-        new_sprint_name = old_sprint_name[0:last_space] + " " + str(new_sprint_number);
-        print(new_sprint_name);
+        current_time_and_day = datetime.now().astimezone().replace(microsecond=0)
+        end_time = current_time_and_day+ timedelta(days=7)
+        url_new_sprint = self.c_jira_url + "/rest/agile/1.0/sprint/"
+        name_len = len(old_sprint_name)
+        last_space = old_sprint_name.rfind(" ",0,name_len)
+        new_sprint_number = int(old_sprint_name[last_space:name_len].strip()) + 1
+        new_sprint_name = old_sprint_name[0:last_space] + " " + str(new_sprint_number)
+        print(new_sprint_name)
         
         payload_new_sprint = json.dumps( {
           "name": new_sprint_name,
@@ -109,13 +109,13 @@ class MyData:
            headers=self.c_common_headers,
            auth=(self.c_user, self.c_apikey)
         )
-        self.debug_log(json.dumps(json.loads(response_new_sprint.text), sort_keys=True, indent=4, separators=(",", ": ")),response_new_sprint);
-        return new_sprint_name,new_sprint_number;
+        self.debug_log(json.dumps(json.loads(response_new_sprint.text), sort_keys=True, indent=4, separators=(",", ": ")),response_new_sprint)
+        return new_sprint_name,new_sprint_number
 
     """ Get project key (typically two letters) for the given board """
     def get_project_key(self):
-        url = self.c_jira_url + "/rest/agile/1.0/board/" + self.c_boar_id;
-        headers = {"Accept": "application/json"};
+        url = self.c_jira_url + "/rest/agile/1.0/board/" + self.c_boar_id
+        headers = {"Accept": "application/json"}
 
         response = requests.get(
            url,
@@ -123,16 +123,16 @@ class MyData:
            auth=(self.c_user, self.c_apikey)
         )
 
-        res = response.text;
-        print(res);
+        res = response.text
+        print(res)
 
         # Save position of the sprint name text to id parameter.
         key_position = res.find("\"projectKey\""); # length of projectKey 10 
         # Remove not needed marks from the begining of the string
         key_name = res[key_position+14::]
         # Remove not needed marks from the end of the sring
-        key_name = key_name[0:key_name.find("\"")];
-        return key_name;
+        key_name = key_name[0:key_name.find("\"")]
+        return key_name
 
     """ Create new story to the new sprint """
     def new_issue(self, project_key,sprint_id,sprint_name):
@@ -144,8 +144,8 @@ class MyData:
                 "customfield_10021": sprint_id
            }
         })
-        url_new_issue = self.c_jira_url + "/rest/api/2/issue/";
-        my_headers = {"Content-Type": "application/json"};
+        url_new_issue = self.c_jira_url + "/rest/api/2/issue/"
+        my_headers = {"Content-Type": "application/json"}
         response_new_issue = requests.post(
            url_new_issue,
            data=payload_new_issue,
@@ -153,7 +153,7 @@ class MyData:
            auth=(self.c_user, self.c_apikey)
         )
 
-        self.debug_log(json.dumps(json.loads(response_new_issue.text), sort_keys=True, indent=4, separators=(",", ": ")),response_new_issue);
+        self.debug_log(json.dumps(json.loads(response_new_issue.text), sort_keys=True, indent=4, separators=(",", ": ")),response_new_issue)
 
     """ Start sprint """
     def start_sprint(self,sprint_id):
@@ -161,7 +161,7 @@ class MyData:
                 "id": sprint_id,
                 "state": "active"
            })
-        start_sprint = self.c_jira_url + "/rest/agile/1.0/sprint/" + str(sprint_id);
+        start_sprint = self.c_jira_url + "/rest/agile/1.0/sprint/" + str(sprint_id)
         response_new_sprint = requests.post(
            start_sprint,
            data=payload_new_issue,
@@ -169,16 +169,15 @@ class MyData:
            auth=(self.c_user, self.c_apikey)
         )
         
-        self.debug_log(json.dumps(json.loads(response_new_sprint.text), sort_keys=True, indent=4, separators=(",", ": ")),response_new_sprint);
+        self.debug_log(json.dumps(json.loads(response_new_sprint.text), sort_keys=True, indent=4, separators=(",", ": ")),response_new_sprint)
 
     def update_sprints(self):
-        active_sprint_id, active_sprint_name  = self.get_sprint("active");
-        self.close_sprint(active_sprint_id);
+        active_sprint_id, active_sprint_name  = self.get_sprint("active")
+        self.close_sprint(active_sprint_id)
         
-        self.create_sprint(active_sprint_name);
-        future_sprint_id, future_sprint_name  = self.get_sprint("future");
+        self.create_sprint(active_sprint_name)
+        future_sprint_id, future_sprint_name  = self.get_sprint("future")
                                                              
-        project_key = self.get_project_key();
-        self.new_issue(project_key,int(future_sprint_id),future_sprint_name);
-        self.start_sprint(int(future_sprint_id));
-
+        project_key = self.get_project_key()
+        self.new_issue(project_key,int(future_sprint_id),future_sprint_name)
+        self.start_sprint(int(future_sprint_id))
