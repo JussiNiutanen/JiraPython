6bae5c201ab23f7ebe2e4161f9c8106908d408c1
Jussi Niutanen <jussi.niutanen@gmail.com>
2022-01-10 12:47:43 +0200

    Update common_test.py

--- auto_sprint/test/common_test.py
+++ auto_sprint/test/common_test.py
@@ -8,7 +8,9 @@ import sys
 from common import MyCommonFunc
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname("auto_sprint"), '..')))
 
-TEST_STRING = '"endDate": "2022-01-14T05:33:05.000Z”,” id": 183, “name": "DDL Sprint 45", "originBoardId": 28, "self": "https://niutanen.atlassian.net/rest/agile/1.0/sprint/183", "startDate": "2022-01-07T05:33:05.000Z", "state": "future"'
+TEST_STRING = '"endDate": "2022-01-14T05:33:05.000Z”,” id": 183, “name": "DDL Sprint 45", \
+    "originBoardId": 28, "self": "https://niutanen.atlassian.net/rest/agile/1.0/sprint/183", \
+    "startDate": "2022-01-07T05:33:05.000Z", "state": "future"'
 
 def test_common_get_sprint_details():
     """ Test for MyCommonFunc class """
