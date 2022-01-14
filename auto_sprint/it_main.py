c65c0d68e5edf21963ead7a43f49ba003f55ac36
Jussi Niutanen <jussi.niutanen@gmail.com>
2022-01-14 16:39:51 +0200

    Update it_main.py

--- auto_sprint/it_main.py
+++ auto_sprint/it_main.py
@@ -18,18 +18,9 @@ except IndexError:
 
 mylogin = MyLogin()
 
-#current_state = mylogin.strip_and_casefold(sys.argv[1])
-#new_state = mylogin.strip_and_casefold(sys.argv[2])
+current_state = mylogin.strip_and_casefold(sys.argv[1])
+new_state = mylogin.strip_and_casefold(sys.argv[2])
 
-#active_sprint_id, active_sprint_name = mylogin.my_get_sprint()
-#KEY = mylogin.my_get_issue_key(active_sprint_id,current_state)
-#mylogin.progress_issue(KEY,new_state)
-
-URL= "https://niutanen.atlassian.net/"
-BOARD_ID = "29"
-USER = "jussi.niutanen@gmail.com"
-mydata = MyData(URL,BOARD_ID,USER,"")
-project_key = mydata.get_project_key()
-sprint_id, name = mylogin.my_get_sprint()
-name
-mydata.new_issue(project_key,int(sprint_id),"Issue name for new issue")
+active_sprint_id, active_sprint_name = mylogin.my_get_sprint()
+KEY = mylogin.my_get_issue_key(active_sprint_id,current_state)
+mylogin.progress_issue(KEY,new_state)
