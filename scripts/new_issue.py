ba712245ed53f422f9ca56e12cf348d25c94dd0b
Jussi Niutanen <jussi.niutanen@gmail.com>
2022-02-08 10:32:24 +0200

    Update new_issue.py

--- scripts/new_issue.py
+++ scripts/new_issue.py
@@ -8,13 +8,19 @@ from as_auto_sprint import MyData
 from it_issue_transition import MyLogin
 
 mylogin = MyLogin()
-mydata = MyData(MyData.URL,MyData.BOARD_ID,MyData.USER,"")
+
+try:
+    board_id = sys.argv[1]
+except IndexError:
+    board_id = MyData.BOARD_ID
+
+mydata = MyData(MyData.URL,board_id,MyData.USER,"")
 project_key = mydata.get_project_key()
 sprint_id, name = mylogin.my_get_sprint()
 
 try:
-    name = sys.argv[1]
+    name = sys.argv[2]
 except IndexError:
-    name = "New issue to" + name
+    name = "New issue to " + name
 
 mydata.create_issues(project_key,int(sprint_id),name)
